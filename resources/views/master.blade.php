b2cef710320a3560990d57b234032f68d9aa3f53
Duta Nugraha <duta.nugraha82@gmail.com>
2025-06-05 23:55:04 +0700

    revision v2

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -21,7 +21,21 @@
   <link rel="stylesheet" href="{{ asset('assets/css/plugins/nice-select.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/plugins/swiper-slider.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+  <style>
+   /* Hilangkan background hijau pada navbar */
+    #header,
+    #header::before,
+    #header::after,
+    .header-area,
+    .header-area::before,
+    .header-area::after {
+      background: none !important;
+      background-color: white !important;
+      background-image: none !important;
+      box-shadow: none !important;
+    }
 
+  </style>
   <!--=====  JS SCRIPT LINK =======-->
   <script src="{{ asset('assets/js/plugins/jquery-3-7-1.min.js') }}"></script>
 </head>
@@ -68,38 +82,21 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
-            <div class="header-top-area">
-              <ul class="header-content">
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                      <path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path>
-                    </svg> hrd@misulisuburperdana.com</a> <span> | </span></li>
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                      <path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path>
-                    </svg> misuli_msp@yahoo.co.id</a> <span> | </span></li>
-                <li><a href="tel:(234)345-4574"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                      <path d="M21 16.42V19.9561C21 20.4811 20.5941 20.9167 20.0705 20.9537C19.6331 20.9846 19.2763 21 19 21C10.1634 21 3 13.8366 3 5C3 4.72371 3.01545 4.36687 3.04635 3.9295C3.08337 3.40588 3.51894 3 4.04386 3H7.5801C7.83678 3 8.05176 3.19442 8.07753 3.4498C8.10067 3.67907 8.12218 3.86314 8.14207 4.00202C8.34435 5.41472 8.75753 6.75936 9.3487 8.00303C9.44359 8.20265 9.38171 8.44159 9.20185 8.57006L7.04355 10.1118C8.35752 13.1811 10.8189 15.6425 13.8882 16.9565L15.4271 14.8019C15.5572 14.6199 15.799 14.5573 16.001 14.6532C17.2446 15.2439 18.5891 15.6566 20.0016 15.8584C20.1396 15.8782 20.3225 15.8995 20.5502 15.9225C20.8056 15.9483 21 16.1633 21 16.42Z"></path>
-                    </svg> 021 - 8833 2376</a><span> | </span></li>
-                <li><a href="tel:(234)345-4574"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                      <path d="M21 16.42V19.9561C21 20.4811 20.5941 20.9167 20.0705 20.9537C19.6331 20.9846 19.2763 21 19 21C10.1634 21 3 13.8366 3 5C3 4.72371 3.01545 4.36687 3.04635 3.9295C3.08337 3.40588 3.51894 3 4.04386 3H7.5801C7.83678 3 8.05176 3.19442 8.07753 3.4498C8.10067 3.67907 8.12218 3.86314 8.14207 4.00202C8.34435 5.41472 8.75753 6.75936 9.3487 8.00303C9.44359 8.20265 9.38171 8.44159 9.20185 8.57006L7.04355 10.1118C8.35752 13.1811 10.8189 15.6425 13.8882 16.9565L15.4271 14.8019C15.5572 14.6199 15.799 14.5573 16.001 14.6532C17.2446 15.2439 18.5891 15.6566 20.0016 15.8584C20.1396 15.8782 20.3225 15.8995 20.5502 15.9225C20.8056 15.9483 21 16.1633 21 16.42Z"></path>
-                    </svg> 021 - 8833 2010</a><span> | </span></li>
-                
-              </ul>
-            </div>
             <div class="header-elements">
               <div class="site-logo">
                 <a href="/"><img src="{{ asset('assets/img/logo/logo.png') }}" alt="housebox"></a>
               </div>
               <div class="main-menu">
                 <ul>
-                  <li><a href="/" class="plus">Beranda</a>
+                  <li><a href="/" class="plus {{ Request::is('/') ? 'active' : '' }}">Beranda</a>
                     
                   </li>
-                  <li><a href="/pelayanan-kami" class="plus">Pelayanan Kami</a>
+                  <li><a href="/pelayanan-kami" class="plus {{ Request::is('pelayanan-kami') ? 'active' : '' }}">Pelayanan Kami</a>
                   </li>
                   <li><a href="#" class="plus">Lowongan Kerja</a>
                   </li>
-                  <li><a href="/tentang-kami" class="plus">Tentang Kami</a></li>
-                  <li><a href="/kontak-kami" class="plus">Kontak Kami</a></li>
+                  <li><a href="/tentang-kami" class="plus {{ Request::is('tentang-kami') ? 'active' : '' }}">Tentang Kami</a></li>
+                  <li><a href="/kontak-kami" class="plus {{ Request::is('kontak-kami') ? 'active' : '' }}">Kontak Kami</a></li>
                 </ul>
               </div>
             </div>
@@ -107,7 +104,7 @@
         </div>
       </div>
     </div>
-  </header>
+</header>
   <!--=====HEADER END =======-->
 
   <!--===== MOBILE HEADER STARTS =======-->
@@ -143,7 +140,7 @@
     </div>
     <div class="mobile-nav mobile-nav1">
       <ul class="mobile-nav-list nav-list1">
-        <li><a class="active" href="/">Beranda</a></li>
+        <li><a class="plus active" href="/">Beranda</a></li>
         <li><a href="/pelayanan-kami">Pelayanan Kami</a></li>
         <li><a href="#">Lowongan Kerja</a></li>
         <li><a href="/tentang-kami">Tentang Kami</a></li>
